dd21b822292eae300422ca9fd7ae04d6233bf2ae
Vitalii Budnik <nekrich@users.noreply.github.com>
2024-10-01 16:52:05 +0300

    chore: rubocop

--- fastlane/lib/fastlane/version.rb
+++ fastlane/lib/fastlane/version.rb
@@ -3,5 +3,5 @@ module Fastlane
   SUMMARY = "The easiest way to build and release mobile apps.".freeze
   DESCRIPTION = "The easiest way to automate beta deployments and releases for your iOS and Android apps".freeze
   MINIMUM_XCODE_RELEASE = "7.0".freeze
-  RUBOCOP_REQUIREMENT = '1.50.2'.freeze
+  RUBOCOP_REQUIREMENT = '1.66.1'.freeze
 end
